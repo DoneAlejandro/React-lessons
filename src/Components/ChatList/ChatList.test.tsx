49a8f8d901212df4ca5afe5ebf16cdc9596f88df
Alejandro <DonAlejandroCarlos@yandex.ru>
2022-10-05 15:08:30 +0300

    lesson-7

--- src/Components/ChatList/ChatList.test.tsx
+++ src/Components/ChatList/ChatList.test.tsx
@@ -1,7 +1,7 @@
 import { ChatList } from "./ChatList";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 jest.mock("nanoid", () => {
   return { nanoid: () => "1234" };
@@ -14,26 +14,32 @@ describe("ChatList", () => {
   it("expect render chats", () => {
     render(<ChatList />);
   });
-  //   it("the chat list contains 3 chats", () => {
-  //     const chats = [
-  //       {
-  //         id: "1",
-  //         name: "Чат 1",
-  //       },
-  //       {
-  //         id: "2",
-  //         name: "Чат 2",
-  //       },
-  //       {
-  //         id: "3",
-  //         name: "Чат 3",
-  //       },
-  //     ];
-  //     render(
-  //       <BrowserRouter>
-  //         <ChatList />
-  //       </BrowserRouter>
-  //     );
-  //     expect(screen.getAllByTestId("list").length).toBe(3);
-  //   });
+  it("the chat list contains 3 chats", () => {
+    const chats = [
+      {
+        id: "1",
+        name: "Чат 1",
+      },
+      {
+        id: "2",
+        name: "Чат 2",
+      },
+      {
+        id: "3",
+        name: "Чат 3",
+      },
+    ];
+    render(
+      <BrowserRouter>
+        <ul>
+          {chats.map((chatItem) => (
+            <li key={chatItem.id} data-testid="list">
+              <a>{chatItem.name}</a>
+            </li>
+          ))}
+        </ul>
+      </BrowserRouter>
+    );
+    expect(screen.getAllByTestId("list").length).toBe(3);
+  });
 });
